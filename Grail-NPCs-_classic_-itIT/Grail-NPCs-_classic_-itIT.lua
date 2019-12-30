e7ff9a6e576a533134220a87cfa4a1d291b820d5
Nerillida <55858788+Nerillida@users.noreply.github.com>
2019-12-30 21:38:55 +0000

    Winter Veil
    Some updates and corrections

--- Grail-NPCs-_classic_-itIT/Grail-NPCs-_classic_-itIT.lua
+++ Grail-NPCs-_classic_-itIT/Grail-NPCs-_classic_-itIT.lua
@@ -11,6 +11,12 @@ interface = tonumber(interface)
 
 if release >= 0 then
 G[0]='Se stesso'
+G[1180715]='Macchina Conserva Agrifoglio'
+G[1180743]='Regalo Incartato con Cura'
+G[1180746]='Dono Dolcemente Scosso'
+G[1180747]='Regalo Incartato Gioiosamente'
+G[1180748]='Regalo Ticchettante'
+G[1180793]='Dono Festivo'
 end
 
 --	End of localized NPC names
