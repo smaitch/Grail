f513258da895982bbd3fb4ee33ad9d7674399eef
Scott Harrison <scott@mithrandir.com>
2021-06-13 14:48:47 -0400

    Updates some quest/NPC information for BCC including some deDE and frFR localizations.

--- Grail-Quests/Grail-Quests-_retail_.lua
+++ Grail-Quests/Grail-Quests-_retail_.lua
@@ -440,7 +440,7 @@ G[45]='FA L1 A:1000055 T:1000056 P:37'
 G[46]='FA L1 A:1203733 T:261'
 G[47]='FA L1 A:241 T:241'
 G[52]='FA L1 A:261 T:261'
-G[54]='L1 A:197 T:240 P:26390'	-- TODO: Determine what is I: because Saab did not get this offered
+G[54]='FA L1 A:197 T:240 P:26390'	-- TODO: Determine what is I: because Saab did not get this offered
 G[56]='L4632 E31961'
 G[57]='L4634 E32305'
 G[58]='L4638 E31961'
@@ -35361,10 +35361,6 @@ G[529428]='HW L2560 A:1209094 T:54141,54146 P:29427 I:29428 Y:29428'	-- assumpti
 G[600000]='SP L0 A:-17 P:FA+35884|37196+I176111,FA+X35884+X37196,FH+34446|37190+I176111,FH+X34446+X37190'	-- Both faction Blasted Lands pre-Iron Horde quest requirement
 G[600001]='SP FA L0 A:-17 P:35884|37196+I176111,X35884+X37196'	-- Alliance Blasted Lands pre-Iron Horde quest requirement
 G[600002]='SP FH L0 A:-17 P:34446|37190+I176111,X34446+X37190'	-- Horde Blasted Lands pre-Iron Horde quest requirement
-end
-
-if release >= 38556 then
-G[54]='FA L1 A:197 T:240 P:26390'	-- TODO: Determine what is I: because Saab did not get this offered
 end
 
 -- Release information
