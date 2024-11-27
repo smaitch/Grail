ebd1c502d3694d02031e9da871ce633fb3896fdf
Scott Harrison <scott@mithrandir.com>
2024-11-27 17:26:39 -0500

    Changed the check for the main quests to be code based instead of release number based.

--- Grail/Quests/Grail-Quests-_retail_.lua
+++ Grail/Quests/Grail-Quests-_retail_.lua
@@ -237,7 +237,7 @@ local _, release, _, interface = GetBuildInfo()
 release = tonumber(release)
 interface = tonumber(interface)
 
-if interface < 100207 then return end
+if not Grail.existsMainline then return end
 
 Grail_Quests_File_Version = 016
 
